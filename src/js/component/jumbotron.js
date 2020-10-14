d1fe0872d297c180fa75500e8c9ea2108b2a29b0
MLopez8282 <saltairpix@gmail.com>
2020-10-14 11:22:26 +0000

    text

--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -3,7 +3,14 @@ import React from "react";
 const tagline = {
 	title: "DINOSAURS That Can Eat People",
 	text:
-		"A newly discovered predatory dinosaur from Utah is so large that it may represent the biggest species of meat-eating dino that ever lived in North America.The dinosaur, Siats meekerorum is described in the latest issue of Nature Communications. The specimen was just a juvenile, but conservatively it measured at least 30 feet long and weighed 9,000 pounds..",
+		"A newly discovered predatory dinosaur from Utah is so large that it may represent the ",
+	text2:
+		"biggest species of meat-eating dino that ever lived in North America.The dinosaur",
+	text3:
+		"Siats meekerorum is described in the latest issue of Nature Communications. The",
+	text4:
+		"specimen was just a juvenile, but conservatively it measured at least 30 feet long",
+	text5: "and weighed 9,000 pounds..",
 	button: {
 		url:
 			"https://www.foxnews.com/science/man-eating-monster-dino-nearly-wiped-out-t-rex",
@@ -15,12 +22,22 @@ export const Jumbotron = () => {
 	return (
 		<div className="container mt-1 mb-1 p-1">
 			<div className="row">
-				<div className="col-12  m-auto">
-					<div className="jumbotron mb-0 ml-2">
-						<h1 className="title">{tagline.title}</h1>
-						<p className="text">{tagline.text}</p>
+				<div className="col-12  m-auto  ">
+					<div className="jumbotron pt-4">
+						<h1 className="title ">{tagline.title}</h1>
+						<p className="text  ">
+							{tagline.text}
+							<br />
+							{tagline.text2}
+							<br />
+							{tagline.text3}
+							<br />
+							{tagline.text4}
+							<br />
+							{tagline.text5}
+						</p>
 						<a
-							className="button jumbotron-button btn mt-4 btn-lg bg-dark text-white"
+							className="button jumbotron-button btn mt-3 btn-lg bg-dark text-white"
 							href={tagline.button.url}
 							role="button">
 							{tagline.button.label}
